2dafd7ca019ea39e166c6501313b7995d77fac0c
tajansari612 <154970584+tajansari612@users.noreply.github.com>
2024-01-01 18:35:51 +0530

    modified 01_sumOfAllElementsInAnArray

--- DSA/03_Arrays/ArrayQuestions/01_sumOfAllElementsInAnArray.cpp
+++ DSA/03_Arrays/ArrayQuestions/01_sumOfAllElementsInAnArray.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int sumOfAll(int arr[],int n){
+int sumOfAllElementsInArray(int arr[],int n){
     int ans = 0;
     for(int i=0;i<n;i++){
         ans+=arr[i];
@@ -11,15 +11,15 @@ int sumOfAll(int arr[],int n){
 }
 int main(){
     int size;
-    int arr[size];
     cout<<"Enter the size of the array :";
     cin>>size;
+    int arr[size];
     cout<<"Enter elements :";
     for(int i=0;i<size;i++){
         cin>>arr[i];
     }
 
-    int sum = sumOfAll(arr,size);
+    int sum = sumOfAllElementsInArray(arr,size);
     cout<<"Sum of all elements of array is :"<<sum<<endl;
     return 0;
 }
