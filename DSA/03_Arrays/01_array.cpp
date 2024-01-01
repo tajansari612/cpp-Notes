86a8a0910d6ba70054417934accafeddb186403a
tajansari612 <154970584+tajansari612@users.noreply.github.com>
2024-01-01 17:40:34 +0530

    modified 01_array file

--- DSA/03_Arrays/01_array.cpp
+++ DSA/03_Arrays/01_array.cpp
@@ -23,12 +23,13 @@ using namespace std;
 
 int main(){
     int arr[] = {2,4,5,6,7};
-    int arr2[5] = {1,2,3,4,5};
-    for(int i=0;i<5;i++){
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
+    cout<<arr[0]<<" ";
+    cout<<arr[1]<<" ";
+    cout<<arr[2]<<" ";
+    cout<<arr[3]<<" ";
+    cout<<arr[4]<<endl;
 
+    int arr2[5] = {1,2,3,4,5};
     for(int i=0;i<5;i++){
         cout<<arr2[i]<<" ";
     }
