d6d57e873030badb8318e765e8ff3ac27eaa774e
tajansari612 <154970584+tajansari612@users.noreply.github.com>
2024-01-03 16:36:45 +0530

    added two new methods reverse and sort

--- DSA/03_Arrays/02_ImpArrayMethod.cpp
+++ DSA/03_Arrays/02_ImpArrayMethod.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 void disp(int arr[],int n){
@@ -16,7 +17,7 @@ void disp(char arr[],int n){
 int main(){
     //Some Important!!! Array methods
 
-    //01 fill_n(name_of_array,size_upto_which_array_initialize,element)
+    //01 fill_n(arr,k,x)
     //Iniatializing array with a choosen value
     int arr[18];
     fill_n(arr,16,-5);
@@ -26,10 +27,19 @@ int main(){
     fill_n(arr2,5,'f');
     disp(arr2,13);
 
-    //02 fill(name_of_array,name_of_array+size_upto_which_value_to_set,value_or_element);
+    //02 fill(arr,arr+k,x);
     int arr3[12];
     fill(arr3,arr3+8,5);
     disp(arr3,12);
+
+    //03 reverse(arr,arr+n)   //reverse the array
+    int arr4[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
+    reverse(arr4,arr4+14);
+    disp(arr4,14);
+
+    //04 sort(arr,arr+n);   //sorts the array in ascending order
+    sort(arr4,arr4+14);
+    disp(arr4,14);
 
     return 0;
 }
