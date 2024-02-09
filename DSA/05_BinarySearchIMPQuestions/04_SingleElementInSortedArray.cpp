9ee28d11e4e845fb612108fd9122cd46d73c437b
tajansari612 <tajansari612@gmail.com>
2024-02-09 22:28:30 +0530

    modified

--- DSA/05_BinarySearchIMPQuestions/04_SingleElementInSortedArray.cpp
+++ DSA/05_BinarySearchIMPQuestions/04_SingleElementInSortedArray.cpp
@@ -1,50 +1,44 @@
 #include<iostream>
 using namespace std;
-long long pow(int n,int m){
-    long long ans=n;
-    for(int i=1;i<m;i++){
-        ans*=n;
+
+int singleNonDuplicate(int arr[],int n)
+{
+	// Write your code here
+    if(n==1){
+        return arr[0];
     }
-    return ans;
-}
-int MthRoot(int n, int m) {
-  // Write your code here.
-  if(m==1){
-    return n;
-  }
-  if(n==1){
-        return 1;
+    if(arr[0]!=arr[1]){
+        return arr[0];
     }
-    if(n==0){
-        return 0;
+    if(arr[n-1]!=arr[n-2]){
+        return arr[n-1];
     }
-    int i=0;
-    int j=n;
-    while(i<j){
-        long long mid=i+(j-i)/2;
-        if(pow(mid,m) == n){
-            return mid;
-        }else if(pow(mid,m)>n){
+	int i=0;
+    int j=n-1;
+    int mid;
+    while(i<=j){
+        mid = i + (j-i)/2;
+        if(arr[mid]!=arr[mid-1] && arr[mid]!=arr[mid+1]){
+            return arr[mid];
+        }else if((arr[mid]==arr[mid-1] && mid%2==0) || (arr[mid]==arr[mid+1] && mid%2==1)){
             j=mid-1;
         }else{
             i=mid+1;
         }
     }
-    
-    if(pow(i,m)==n){
-        return i;
-    }
     return -1;
 }
 int main(){
     int n;
-    cout<<"Enter the number to find the root :";
+    cout<<"Enter the size of array :";
     cin>>n;
-    int m;
-    cout<<"Enter the mth root :";
-    cin>>m;
+    int arr[n];
+    cout<<"Enter the elements in increasing rotated order with one element is single and other elements are twice :";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
     
-    long long Mthroot = MthRoot(n,m);
-    cout<<"The root element is :"<<Mthroot<<endl;
+    int singleElement = singleNonDuplicate(arr,n);
+    cout<<"Pivot is present at the index :"<<singleElement<<endl;
     return 0;
 }
