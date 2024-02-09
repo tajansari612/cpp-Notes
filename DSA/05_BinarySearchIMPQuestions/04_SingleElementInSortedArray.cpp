07061f22587b6fc601fd935a1cdfa7d7470d8a99
tajansari612 <tajansari612@gmail.com>
2024-02-09 20:24:14 +0530

    new file added

--- DSA/05_BinarySearchIMPQuestions/04_SingleElementInSortedArray.cpp
+++ DSA/05_BinarySearchIMPQuestions/04_SingleElementInSortedArray.cpp
@@ -1,42 +1,50 @@
 #include<iostream>
 using namespace std;
-
-int singleElement(int arr[],int n){
-    if(n==1){
-        return arr[0];
+long long pow(int n,int m){
+    long long ans=n;
+    for(int i=1;i<m;i++){
+        ans*=n;
     }
-    if(arr[0]!=arr[1]){
-        return arr[0];
+    return ans;
+}
+int MthRoot(int n, int m) {
+  // Write your code here.
+  if(m==1){
+    return n;
+  }
+  if(n==1){
+        return 1;
     }
-    if(arr[n-1]!=arr[n-2]){
-        return arr[n-1];
+    if(n==0){
+        return 0;
     }
-	int i=0;
-    int j=n-1;
-    int mid;
-    while(i<=j){
-        mid = i + (j-i)/2;
-        if(arr[mid]!=arr[mid-1] && arr[mid]!=arr[mid+1]){
-            return arr[mid];
-        }else if((arr[mid]==arr[mid-1] && mid%2==0) || (arr[mid]==arr[mid+1] && mid%2==1)){
+    int i=0;
+    int j=n;
+    while(i<j){
+        long long mid=i+(j-i)/2;
+        if(pow(mid,m) == n){
+            return mid;
+        }else if(pow(mid,m)>n){
             j=mid-1;
         }else{
             i=mid+1;
         }
     }
+    
+    if(pow(i,m)==n){
+        return i;
+    }
     return -1;
 }
 int main(){
     int n;
-    cout<<"Enter the size of array :";
+    cout<<"Enter the number to find the root :";
     cin>>n;
-    int arr[n];
-    cout<<"Enter the elements in increasing order with one element single and other elements is twice :";
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
+    int m;
+    cout<<"Enter the mth root :";
+    cin>>m;
     
-    int sinlgeEle = singleElement(arr,n);
-    cout<<"The single element is :"<<sinlgeEle<<endl;
+    long long Mthroot = MthRoot(n,m);
+    cout<<"The root element is :"<<Mthroot<<endl;
     return 0;
 }
