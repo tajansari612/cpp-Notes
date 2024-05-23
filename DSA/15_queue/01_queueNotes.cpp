e22a849554dc3ca50822ea4879c42e649de70753
tajansari612 <tajansari612@gmail.com>
2024-05-23 17:33:20 +0530

    new file queueNotes added

---
+++ DSA/15_queue/01_queueNotes.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+// This data structure works on the FIFO technique, where FIFO stands for First In
+// First Out. The element which was first inserted will be extracted at the first
+// and so on. There is an element called as 'front' which is the element at the
+// front most position or say the first position, also there is an element called
+// as 'rear' which is the element at the last position. In normal queues insertion
+// of elements take at the rear end and the deletion is done from the front.
+
+// The containers should have a support for the following list of operations:
+// empty
+// size
+// push_back
+// pop_front
+// front
+// back
+
+// empty:
+// The function is used to test for the emptiness of a queue. If the queue is
+// empty the function returns true else false.
+
+// size:
+// The function returns the size of the queue container, which is a measure of the
+// number of elements stored in the queue.
+
+// push_back/enqueue:
+// The function is used for the insertion of a new element at the rear end of the
+// queue.
+
+// pop_front/dequeue:
+// The function is used for the deletion of element; the element in the queue is
+// deleted from the front end.
+
+// front:
+// The function is used to access the front element of the queue. The element
+// plays a very important role as all the deletion operations are performed at
+// the front element.
+
+// back:
+// The function is used to access the rear element of the queue. The element plays
+// a very important role as all the insertion operations are performed at the rear
+// element.
+int main(){
+    
+    return 0;
+}
