8cdd7666002efe5294276a1a15d7b634a12785ae
tajansari612 <tajansari612@gmail.com>
2024-10-29 12:03:41 +0530

    new topic added

---
+++ OOPs/04_fourPillarsOfOOPs.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+// Abstraction:
+// The concept allows us to hide the implementation from the user but shows 
+// only essential information to the user. Using the concept developer can 
+// easily make changes and added over time.
+
+// Encapsulation:
+// Encapsulation is a mechanism that allows us to bind data and functions of a 
+// class(methods) into an entity. 
+// It protects data and functions from outside interference and misuse called 
+// as data hiding by making data & methods as private or protected. 
+// Therefore, it also provides security. 
+// A class is the best example of encapsulation.
+
+// Inheritance:
+// The concept allows us to inherit or acquire the properties of an existing 
+// class (parent class) into a newly created class (child class). 
+// It is known as inheritance. It provides code reusability.
+
+// Polymorphism:
+// The word polymorphism is derived from the two words i.e. poly and morphs. 
+// Poly means many and morphs means forms. It allows us to create methods with 
+// the same name but different method signatures. It allows the developer to 
+// create clean, sensible, readable, and resilient code.
+
+int main(){
+    
+    return 0;
+}
