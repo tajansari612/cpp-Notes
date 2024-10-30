f80e6097dcf59216f6c48a5be00e2ca03ca23cbd
tajansari612 <tajansari612@gmail.com>
2024-10-30 11:06:33 +0530

    theory added

--- OOPs/08_copyCostructor.cpp
+++ OOPs/08_copyCostructor.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
 using namespace std;
 
+// In C++, a copy constructor is a special constructor used to create a new 
+// object as a copy of an existing object. The copy constructor is 
+// automatically called when an object is:
+// 1) Initialized with another object of the same type (e.g., MyClass obj2 = obj1;).
+// 2) Passed by value to a function.
+// 3) Returned by value from a function.
 
+// Default Copy Constructor
+// If you don’t define a copy constructor, the compiler provides a default one 
+// that performs a shallow copy. 
+// However, with classes that have dynamically allocated memory, this can lead 
+// to issues like double deletion, where both objects try to delete the same 
+// memory. 
+// Hence, a custom copy constructor is often needed in such cases.
 class student{
     //data members/properties
     int rollNo;
