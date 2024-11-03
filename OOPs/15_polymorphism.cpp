a5935fc6abf4aae104d5c1bb9e87fd82fe8924d5
tajansari612 <tajansari612@gmail.com>
2024-11-04 00:07:55 +0530

    new topic added

---
+++ OOPs/15_polymorphism.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+// Polymorphism is an important concept of object-oriented programming. 
+// It simply means more than one form. That is, the same entity (function or 
+// operator) behaves differently in different scenarios depending on the context
+// they are used 
+
+// For example,
+// The + operator in C++ is used to perform two specific functions. 
+// When it is used with numbers (integers and floating-point numbers), 
+// it performs addition.
+// And when we use the + operator with strings, it performs string 
+// concatenation.
+
+// We can implement polymorphism in C++ using the following ways:
+// Function overloading(method oveloading if inside the class)
+// Operator overloading
+// constructor overloading
+// Function overriding
+// Virtual functions
+
+
+//Types of polymorphism
+// There are two main types of polymorphism in C++:
+
+// 1. Compile-time polymorphism (also known as static polymorphism)
+// 2. Runtime polymorphism (also known as dynamic polymorphism)
+
+//01 Compile time polymorphism
+// Compile-time polymorphism is achieved through function overloading and 
+// operator overloading. It occurs during compilation and is also known as 
+// static binding.
+// i. Function overloading
+// ii. Operator overloading
+// iii. constructor overloading 
+
+//02 Run time polymorphism
+// Runtime polymorphism is achieved through inheritance and virtual functions. 
+// It occurs during program execution and is also known as dynamic binding.
+// i. Method overriding
+// ii. Virtual functions
+
+int main(){
+    
+    return 0;
+}
