6f7908f587b295f62c2040e287b8b8aceed1f133
tajansari612 <tajansari612@gmail.com>
2024-11-03 17:22:46 +0530

    new topic added

---
+++ OOPs/14_typesOfInheritance.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+// Types of inheritance
+//01 Single inheritance
+// Single inheritance is defined as the inheritance in which a derived
+// class is inherited from the only one base class.
+
+//02 Multilevel inheritance
+// When one class inherits another class which is further inherited by
+// another class, it is known as multi level inheritance in C++.
+// Inheritance is transitive so the last derived class acquires all the
+// members
+
+//03 Multiple Inheritance
+// Multiple Inheritance is a feature of C++ where a class can inherit from
+// more than one class. i.e one subclass is inherited from more than one
+// base class.rs of all its base classes.
+
+//04 Hierarchical Inheritance
+// Hierarchical inheritance is defined as the process of deriving more than
+// one class from a base class.
+
+//05 Hybrid Inheritance
+// Hybrid inheritance is a combination of more than one type of inheritance.
+
+
+int main(){
+    
+    return 0;
+}
